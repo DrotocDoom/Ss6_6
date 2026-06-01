e324d2d59d21eb4790cd0290c251b6638d20e32d
DrotocDoom <lenguyen14032003@gmail.com>
2026-06-01 11:17:29 +0700

    6

---
+++ Ex6.sql
@@ -0,0 +1,33 @@
+
+CREATE TABLE Orders (
+                        id SERIAL PRIMARY KEY,
+                        customer_id INT,
+                        order_date DATE,
+                        total_amount NUMERIC(10,2)
+);
+
+SELECT
+    SUM(total_amount) AS total_revenue,
+    COUNT(id) AS total_orders,
+    AVG(total_amount) AS average_order_value
+FROM Orders;
+
+SELECT
+    EXTRACT(YEAR FROM order_date) AS order_year,
+    SUM(total_amount) AS total_revenue
+FROM Orders
+GROUP BY EXTRACT(YEAR FROM order_date)
+ORDER BY order_year;
+
+
+SELECT
+    EXTRACT(YEAR FROM order_date) AS order_year,
+    SUM(total_amount) AS total_revenue
+FROM Orders
+GROUP BY EXTRACT(YEAR FROM order_date),
+HAVING SUM(total_amount) > 50000000
+ORDER BY order_year;
+
+SELECT * FROM Orders
+ORDER BY total_amount DESC
+LIMIT 5;
